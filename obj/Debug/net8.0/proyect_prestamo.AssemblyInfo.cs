58808c2399c559659132af2f3e2473cf833726a8
RamonMarte <martejavielr@gmail.com>
2025-10-26 09:42:26 -0400

    Agregando Dockerfile para Railway

--- obj/Debug/net8.0/proyect_prestamo.AssemblyInfo.cs
+++ obj/Debug/net8.0/proyect_prestamo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyect_prestamo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a29be32fe01ff6e1a9e06b682e5557542f1d0957")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyect_prestamo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyect_prestamo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
